fdc846a18b8b8c8197258d7185467499f2aa343f
Andromeda <christeenfabian@gmail.com>
2026-04-16 20:45:45 +0200

    attempt 2 open pr

--- client/lib/notifications/fragment-updates/CanvaNotification_MoveXY.tsx
+++
@@ -1,9 +0,0 @@
-const CanvaNotification_MoveXY = () => {
-  if (Notification.permission === "granted") {
-    new Notification("Text fragment's coordinate update ", {
-      body: `A text fragment's coordinates has been updated!`,
-    });
-  }
-};
-
-export default CanvaNotification_MoveXY;
